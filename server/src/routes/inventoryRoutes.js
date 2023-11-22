b2a9284c858c532f5a50a24997edb3166b90745c
catonnet <julien.catonnet@essec.edu>
2023-11-22 00:21:10 +0000

    add authorization on inventoy routes

--- server/src/routes/inventoryRoutes.js
+++ server/src/routes/inventoryRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { addInventoryItem, getUserInventoryItems } from '../controllers/inventoryController.js';
+import { authMiddleware } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/inventory', addInventoryItem);
-router.get('/inventory/:userId', getUserInventoryItems);
+router.post('/inventory', authMiddleware, addInventoryItem);
+router.get('/inventory/:userId', authMiddleware, getUserInventoryItems);
 
 export default router;
